829d53ded9bf98383734be7cb49c534591a59c13
Yannick Longval <yannicklongval2014@gmail.com>
2022-01-23 13:16:58 -0500

    Progress
    Make a bit more responsiveness, fixed a couple bugs, added cool character design.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const highlightMenu = () => {
     let scrollPos = window.scrollY;
     console.log(scrollPos);
     // Adds 'highlight' class to menu items
-    if(window.innerWidth > 960 && scrollPos <  0.5 * document.documentElement.clientHeight) {
+    if(scrollPos <  0.5 * document.documentElement.clientHeight) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         codingMenu.classList.remove('highlight')
@@ -16,7 +16,7 @@ const highlightMenu = () => {
         document.documentElement.style.setProperty('--text', '#ffffff')
         document.documentElement.style.setProperty('--comp', '#19EF82')
         return;
-    } else if (window.innerWidth > 960 && scrollPos <=  1.5 * document.documentElement.clientHeight) {
+    } else if (scrollPos <=  1.5 * document.documentElement.clientHeight) {
         aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
         codingMenu.classList.remove('highlight')
@@ -24,7 +24,7 @@ const highlightMenu = () => {
         document.documentElement.style.setProperty('--text', '#323044')
         document.documentElement.style.setProperty('--comp', '#ffffff')
         return;
-    } else if (window.innerWidth > 960 && scrollPos >=  1.5 * document.documentElement.clientHeight) {
+    } else if (scrollPos >=  1.5 * document.documentElement.clientHeight) {
         codingMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         homeMenu.classList.remove('highlight');
